943900f80d237dfb1ce739acd14f8561074a2456
jkulunk-cl <jkulunk@corelogic.com>
2019-05-17 14:55:31 -0700

    global alert

--- global-alert/global-alert.component.js
+++ global-alert/global-alert.component.js
@@ -1,9 +1,15 @@
 $(function () {
 
     $("#button1").on("click", function(){
+        
+        var pause = 6000;
 
         var alertContainer = $("#alert1");        
         $(alertContainer).slideDown();
+
+        setTimeout(function(){
+            $(alertContainer).slideUp();
+        }, pause);
 
         var closeBtn = $(alertContainer).find(".close");
         $(closeBtn).on("click", function(){
@@ -11,7 +17,5 @@ $(function () {
         });
 
     });
-
-
     
 });
