3f416adc955d42611e6b14de202fd7cd395f05bc
jkulunk-cl <jkulunk@corelogic.com>
2019-05-17 15:30:08 -0700

    rework

--- global-alert/global-alert.component.js
+++ global-alert/global-alert.component.js
@@ -2,14 +2,20 @@ $(function () {
 
     $("#button1").on("click", function(){
 
-        var pause = 10000;
+        
+        // Rework. add a data-target to the alert ID
+        var alertContainer = $("#alert1");
+        var timeout = alertContainer.attr("data-timeout");
+        
+        // Rework. problem with adding multiple on multiple clicks.
+        var message = $(this).attr("data-message");
 
-        var alertContainer = $("#alert1");        
+        $(alertContainer).prepend(message);
         $(alertContainer).slideDown();
 
         setTimeout(function(){
             $(alertContainer).slideUp();
-        }, pause);
+        }, timeout);
 
         var closeBtn = $(alertContainer).find(".close");
         $(closeBtn).on("click", function(){
