b64b36b1eb06f50ec6c8d5385d06fc00fa6c739f
jkulunk-cl <jkulunk@corelogic.com>
2019-05-23 10:07:39 -0700

    IE11 fixes

--- map-layers/map-layers.component.js
+++ map-layers/map-layers.component.js
@@ -5,28 +5,26 @@ $(function () {
         "title": "Zoom-in to enable"
     });
 
-
-    /*
-
     // Toggle Date Range Options with jQuery
 
-     $(".mls_status_option").change(function() {
-        var target = this.getAttribute('data-target');
-        $('#' + target).toggle();
-    });
+    //  $(".mls_status_option").change(function() {
+    //     var target = this.getAttribute('data-target');
+    //     $('#' + target).toggle();
+    // });
 
-    */ 
+});
 
+
+document.addEventListener("DOMContentLoaded", function (event) { // Page load event
 
     // Toggle Date Range Options without jQuery
     var mlsStatusOptions = document.querySelectorAll('.mls_status_option');
 
-    mlsStatusOptions.forEach(function (el) {
-        el.addEventListener('change', function () {
+    for (var i = 0; i < mlsStatusOptions.length; i++) {
+        mlsStatusOptions[i].addEventListener('change', function () {
             var dataTarget = this.getAttribute('data-target');
             document.getElementById(dataTarget).classList.toggle('show');
         });
-    });
+    }
 
-    
 });
