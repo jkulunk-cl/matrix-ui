706b9f4ae075f36ec2aaaca1069aadcf813f96a0
John Kulunk <jkulunk@corelogic.com>
2019-12-05 15:51:09 -0800

    footer

--- matrix-app/src/app/footer/footer.component.ts
+++ matrix-app/src/app/footer/footer.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  disclaimerText:string = `This MLS disclaimer text is independently owned and operated. Your membership to
+  this example provides you with certain services and access to, among other things, this and that.
+  This agreement grants you a license to do nothing and is subject to the terms and conditions`;
+
+  matrixVersion:string = "Matrix 9.0";
+
   constructor() { }
 
   ngOnInit() {
