9587009af4b210edddc6353f2410e12bf504f3cd
jkulunk-cl <jkulunk@corelogic.com>
2019-12-09 09:49:00 -0800

    quick add

---
+++ EventApp/events/events-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
+
+// declare let toastr
+
+@Component({
+    selector: 'app-events-list',
+    templateUrl: 'events-list.component.html',
+    styleUrls: ['events-list.component.css']
+})
+export class EventsListComponent implements OnInit {
+    events: any[];
+
+    constructor(private eventService: EventService, private toastr: ToastrService) {
+    }
+
+    ngOnInit() {
+        this.events = this.eventService.getEvents();
+    }
+
+    handleThumbnailClick(eventName) {
+        this.toastr.success(eventName);
+    }
+}
---
+++ EventApp/examples/datatable.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-data-table',
+    templateUrl: 'datatable.component.html'
+
+})
+export class DataTableComponent {}
