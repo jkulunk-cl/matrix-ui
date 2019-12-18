958b0dd2fe5cdb021c3b856aa3da2ca0825e7136
John Kulunk <jkulunk@corelogic.com>
2019-12-18 15:22:22 -0800

    match fonts

--- legacy/notifications/notifications.component.js
+++ legacy/notifications/notifications.component.js
@@ -1,40 +1,21 @@
-var filterMenu = (function(){
-    // Toggle filter menu
-    var filterBtn = document.querySelectorAll('.filter-btn');
+        //
+        // jQuery Popover Function
 
-    for (var i = 0; i < filterBtn.length; i++) {
-        filterBtn[i].addEventListener('click', function () {
-            var dataTarget = this.getAttribute('data-target');
-            document.getElementById(dataTarget).classList.toggle('show');
-        });
-    }
+        var popoverTemplate = '<div class="popover mtx-popover" role="tooltip"><div class="arrow mtx-arrow"></div><h3 class="popover-title mtx-popover-title"></h3><div class="popover-content mtx-popover-content"></div></div>';
+        var popoverContent = $('#ExampleNotificationItems').html();
 
-    // Close menu
-    var handleClose = function(){
-        var filterMenu = document.querySelectorAll('.filter-menu');
-        for (var i = 0; i < filterMenu.length; i++){
-            filterMenu[i].classList.remove('show');
-        }
-    };
+        $("#ExampleNotificationAlert").popover(
+            {
+                html: true,
+                title: 'Notifications',
+                content: popoverContent,
+                template: popoverTemplate,
+                placement: 'bottom',
+                trigger: 'click'
+            }
+        );
 
-    // Selected item
-    var filterItem = document.querySelectorAll('.filter-menu a');
-    
-    for (var i = 0; i < filterItem.length; i++) {
-        filterItem[i].addEventListener('click', function () {
-            var selected = this.innerText;
-            document.getElementById("filter-dropdown").innerText = selected;
-                    //show reset. to redo
-                    // if not top level
-                    //document.getElementById('filter-reset').classList.add('show');
+      // TODO - If count > 0 add "active" class to alert badge
 
-            handleClose();
-        });
-    }
-
-
-
-
-}());
-
-filterMenu();
+    // end Popover
+    //
